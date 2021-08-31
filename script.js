2de8b531783b9d2b858f94c343561d09cf3dc271
Ariel Surco Acho <arielsurcoa@gmail.com>
2021-08-31 14:20:14 -0300

    Add main-section responsive

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburguer = document.querySelector(".nav .hamburguer");
 const navLinks = document.querySelector(".nav .nav-links");
+const body = document.querySelector("body");
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("transform");
     navLinks.classList.toggle("open");
+    body.classList.toggle("hamburguer-open");
 })
