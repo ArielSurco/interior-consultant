fe357397f70cf0e98013645477e87f093a244537
Ariel Surco Acho <arielsurcoa@gmail.com>
2021-09-02 18:02:01 -0300

    Footer temporary fix

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const hamburguer = document.querySelector(".nav .hamburguer");
 const navLinks = document.querySelector(".nav .nav-links");
 const body = document.querySelector("body");
+
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("transform");
     navLinks.classList.toggle("open");
     body.classList.toggle("hamburguer-open");
 })
+
+const positionFooter = () => {
+    const footer = document.querySelector(".footer");
+    footer.style.position = (body.offsetHeight < innerHeight) ? "initial" : "relative";
+}
+
+window.addEventListener("load", positionFooter);
